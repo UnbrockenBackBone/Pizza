92f0e508d36234196a4e2384177a05a09415f8f7
Максим Левчук <levchukmaks2001@gmail.com>
2021-05-14 21:17:54 +0300

    fix

---
+++ Pizza/Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Domain.Entities
+{
+    public class Product
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Категория товара")]
+        public string Category { get; set; }
+
+        [Display(Name = "Название товара")]
+        public string Name { get; set; }
+
+        [Display(Name = "Цена товара")]
+        public int Price { get; set; }
+
+        [Display(Name = "Краткое описание")]
+        public string Subtitle { get; set; }
+
+        [Display(Name = "Титульная картинка")]
+        public string TitleImagePath { get; set; }
+    }
+}
---
+++ Pizza/Domain/PizzaDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pizza.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Domain
+{
+    public class PizzaDbContext : IdentityDbContext<IdentityUser>
+    {
+        public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options) { }
+        public DbSet<Product> TextFields { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                Name = "admin",
+                NormalizedName = "ADMIN"
+            });
+
+            builder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                NormalizedEmail = "admin@admin.com",
+                EmailConfirmed = true,
+                PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "admin"),
+                SecurityStamp = string.Empty
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                UserId = "3b62472e-4f66-49fa-a20f-e7685b9565d8"
+            });
+
+            builder.Entity<Product>().HasData(new Product
+            {
+                Id = Guid.NewGuid(),
+                Category = "Pizza",
+                Name = "Deluxe",
+                Price = 10,
+                Subtitle = "original & favorite specialty pizza topped",
+                TitleImagePath = "~/images/pizza_img2.jpg"
+            }, new Product
+            {
+                Id = Guid.NewGuid(),
+                Category = "Pizza",
+                Name = "Deluxe",
+                Price = 10,
+                Subtitle = "original & favorite specialty pizza topped",
+                TitleImagePath = "~/images/pizza_img2.jpg"
+            });
+        }
+    }
+}
