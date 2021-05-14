74dbeb87039b0c12a9b67ad5e931d9454e6b3e80
Максим Левчук <levchukmaks2001@gmail.com>
2021-05-14 21:32:49 +0300

    add new

---
+++ Pizza/Domain/Repositories/Abstract/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Pizza.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Domain.Repositories.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetProducts();
+        Product GetProductsById(Guid id);
+        Product GetProductsByCategoryName(string category);
+    }
+}
---
+++ Pizza/Domain/Repositories/EntityFramework/EFProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using Pizza.Domain.Entities;
+using Pizza.Domain.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Domain.Repositories.EntityFramework
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly PizzaDbContext context;
+        public EFProductRepository(PizzaDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            return context.PostItems;
+        }
+
+        public Product GetProductsByCategoryName(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductsById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
