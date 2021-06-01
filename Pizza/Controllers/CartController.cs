d006ec532ac900a1538c35cbdbf7d04efd5bb41e
Максим Левчук <levchukmaks2001@gmail.com>
2021-06-01 21:29:06 +0300

    localeStorage

--- Pizza/Controllers/CartController.cs
+++ Pizza/Controllers/CartController.cs
@@ -5,7 +5,6 @@ using System.Security.Claims;
 
 namespace Pizza.Controllers
 {
-    [Authorize]
     public class CartController : Controller
     {
         private readonly DataManager dataManager;
@@ -17,32 +16,32 @@ namespace Pizza.Controllers
         {
             ViewBag.isAut = HttpContext.User.Identity.IsAuthenticated;
             ViewBag.Name = HttpContext.User.Identity.Name;
-            ClaimsPrincipal currentUser = this.User;
-            var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
-            return View(dataManager.UserProducts.GetByUserId(System.Guid.Parse(currentUserID)));
+            return View();
         }
 
-        public IActionResult Add(string id)
+        [HttpPost]
+        public IActionResult Сheckout(int total)
         {
             ViewBag.isAut = HttpContext.User.Identity.IsAuthenticated;
             ViewBag.Name = HttpContext.User.Identity.Name;
-            ClaimsPrincipal currentUser = this.User;
-            var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
-            dataManager.UserProducts.SaveProduct(new UserProducts { Id = new System.Guid(),
-                                                                    IdProduct = System.Guid.Parse(id), 
-                                                                    IdUser = System.Guid.Parse(currentUserID)
-            });
-            return RedirectToAction(nameof(CartController.Index));
+            return View(total);
         }
 
-        public IActionResult Delete(string id)
+        public IActionResult Add(/*string id*/)
         {
             ViewBag.isAut = HttpContext.User.Identity.IsAuthenticated;
             ViewBag.Name = HttpContext.User.Identity.Name;
-            ClaimsPrincipal currentUser = this.User;
-            var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
-            dataManager.UserProducts.DeleteProduct(System.Guid.Parse(id), System.Guid.Parse(currentUserID));
             return RedirectToAction(nameof(CartController.Index));
         }
+
+        //public IActionResult Delete(string id)
+        //{
+        //    ViewBag.isAut = HttpContext.User.Identity.IsAuthenticated;
+        //    ViewBag.Name = HttpContext.User.Identity.Name;
+        //    ClaimsPrincipal currentUser = this.User;
+        //    var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+        //    dataManager.UserProducts.DeleteProduct(System.Guid.Parse(id), System.Guid.Parse(currentUserID));
+        //    return RedirectToAction(nameof(CartController.Index));
+        //}
     }
 }
